0841b671830ab34fa3914dbf5f9cf320de07c60c
Alex Piechowski <alex@piechowski.io>
2022-04-07 17:46:36 -0500

    Add stimulus to default build
    It's nice and easy for js sprinkles

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,1 +1,8 @@
 import * as Turbo from "@hotwired/turbo"
+
+import { Application } from "@hotwired/stimulus"
+
+// import MyController from "./controllers/my_controller"
+
+window.Stimulus = Application.start()
+// Stimulus.register("my-controller", MyController)
