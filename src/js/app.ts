ef26f99b0605c5dbc8a5ec34f88bcc8721bf8368
Alex Piechowski <alex@piechowski.io>
2022-04-09 20:27:04 -0500

    Add prettier
    It makes everything a little prettier
    
    TODO: no semicolons would be nice

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,10 +1,10 @@
 require("@rails/ujs").start();
 
-import * as Turbo from "@hotwired/turbo"
+import * as Turbo from "@hotwired/turbo";
 
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
 // import MyController from "./controllers/my_controller"
 
-window.Stimulus = Application.start()
+window.Stimulus = Application.start();
 // Stimulus.register("my-controller", MyController)
